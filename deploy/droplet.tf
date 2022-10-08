3cb1ade3b8f1086c84b9da113bf9698d475380d0
Dane Petersen <thegreatsunra@gmail.com>
2022-10-08 17:36:32 -0500

    Define droplet resource

---
+++ deploy/droplet.tf
@@ -0,0 +1,8 @@
+resource "digitalocean_droplet" "droplet" {
+  image      = var.droplet_image
+  name       = var.droplet_name
+  region     = var.droplet_region
+  size       = var.droplet_size
+  monitoring = true
+  tags       = var.droplet_tags
+}
--- deploy/inputs.tf
+++ deploy/inputs.tf
@@ -1,3 +1,28 @@
 variable "digital_ocean_token" {
   type = string
 }
+
+variable "droplet_image" {
+  type    = string
+  default = "debian-11-x64"
+}
+
+variable "droplet_name" {
+  type = string
+}
+
+variable "droplet_region" {
+  type    = string
+  default = "nyc1"
+}
+
+variable "droplet_size" {
+  type    = string
+  default = "s-1vcpu-1gb"
+}
+
+variable "droplet_tags" {
+  type    = list(any)
+  default = ["droplet"]
+}
+
