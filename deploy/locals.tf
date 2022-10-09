894d4f94f165efc601ab8df5d6d5e0996913f5eb
Dane Petersen <thegreatsunra@gmail.com>
2022-10-09 14:26:23 -0500

    Add trailing semi-colon, rename nginx templates to prevent auto-import

--- deploy/locals.tf
+++ deploy/locals.tf
@@ -8,8 +8,8 @@ locals {
   }
   user_data_vars = {
     docker_compose_base64   = base64encode(templatefile("./templates/docker-compose.yml.tftpl", local.docker_compose_vars))
-    nginx_http_conf_base64  = base64encode(templatefile("./templates/nginx.http.conf.tftpl", local.nginx_conf_vars))
-    nginx_https_conf_base64 = base64encode(templatefile("./templates/nginx.https.conf.tftpl", local.nginx_conf_vars))
+    nginx_http_conf_base64  = base64encode(templatefile("./templates/nginx.conf.http.tftpl", local.nginx_conf_vars))
+    nginx_https_conf_base64 = base64encode(templatefile("./templates/nginx.conf.https.tftpl", local.nginx_conf_vars))
     public_ssh_key          = var.public_ssh_key
     root_temporary_password = var.root_temporary_password
     user_full_name          = var.user_full_name
