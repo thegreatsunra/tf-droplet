8230e4f27d14bff855415180be601464fc7d773e
Dane Petersen <thegreatsunra@gmail.com>
2022-10-15 16:47:40 -0500

    Apply terraform fmt

--- deploy/locals.tf
+++ deploy/locals.tf
@@ -21,21 +21,21 @@ locals {
   }
 
   user_data_vars = {
-    docker_compose_base64   = base64encode(templatefile("./templates/docker-compose.yml.tftpl", local.docker_compose_vars))
-    docker_volume_path      = local.docker_volume_path
-    nginx_conf_base64 = base64encode(templatefile("./templates/nginx-conf/default.conf.tftpl", local.nginx_conf_vars))
-    nginx_config_general_conf_base64 = base64encode(file("./templates/nginx-conf/modules/general.conf"))
-    nginx_config_security_conf_base64 = base64encode(file("./templates/nginx-conf/modules/security.conf"))
+    docker_compose_base64              = base64encode(templatefile("./templates/docker-compose.yml.tftpl", local.docker_compose_vars))
+    docker_volume_path                 = local.docker_volume_path
+    nginx_conf_base64                  = base64encode(templatefile("./templates/nginx-conf/default.conf.tftpl", local.nginx_conf_vars))
+    nginx_config_general_conf_base64   = base64encode(file("./templates/nginx-conf/modules/general.conf"))
+    nginx_config_security_conf_base64  = base64encode(file("./templates/nginx-conf/modules/security.conf"))
     nginx_config_wordpress_conf_base64 = base64encode(file("./templates/nginx-conf/modules/wordpress.conf"))
-    public_ssh_key          = var.public_ssh_key
-    root_temporary_password = var.root_temporary_password
-    tmp_docker_volume_path  = local.tmp_docker_volume_path
-    tmp_folder_name         = var.tmp_folder_name
-    user_full_name          = var.user_full_name
-    user_temporary_password = var.user_temporary_password
-    username                = var.username
-    volume_mount_path       = local.volume_mount_path
-    volume_name             = local.volume_name
-    wordpress_dotenv_base64 = base64encode(file("./templates/.env"))
+    public_ssh_key                     = var.public_ssh_key
+    root_temporary_password            = var.root_temporary_password
+    tmp_docker_volume_path             = local.tmp_docker_volume_path
+    tmp_folder_name                    = var.tmp_folder_name
+    user_full_name                     = var.user_full_name
+    user_temporary_password            = var.user_temporary_password
+    username                           = var.username
+    volume_mount_path                  = local.volume_mount_path
+    volume_name                        = local.volume_name
+    wordpress_dotenv_base64            = base64encode(file("./templates/.env"))
   }
 }
