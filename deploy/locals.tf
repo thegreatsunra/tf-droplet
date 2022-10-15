101a7fe04547e83a0c4b990ba9eac7ea02f485b6
Dane Petersen <thegreatsunra@gmail.com>
2022-10-15 08:43:14 -0500

    Move nginx configs to a folder

--- deploy/locals.tf
+++ deploy/locals.tf
@@ -21,8 +21,8 @@ locals {
   user_data_vars = {
     docker_compose_base64   = base64encode(templatefile("./templates/docker-compose.yml.tftpl", local.docker_compose_vars))
     docker_volume_path      = local.docker_volume_path
-    nginx_http_conf_base64  = base64encode(templatefile("./templates/nginx.conf.http.tftpl", local.nginx_conf_vars))
-    nginx_https_conf_base64 = base64encode(templatefile("./templates/nginx.conf.https.tftpl", local.nginx_conf_vars))
+    nginx_http_conf_base64  = base64encode(templatefile("./templates/nginx-conf/nginx.conf.http.tftpl", local.nginx_conf_vars))
+    nginx_https_conf_base64 = base64encode(templatefile("./templates/nginx-conf/nginx.conf.https.tftpl", local.nginx_conf_vars))
     public_ssh_key          = var.public_ssh_key
     root_temporary_password = var.root_temporary_password
     tmp_folder_name         = var.tmp_folder_name
