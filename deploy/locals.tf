0a3aeedd57567aeba07601297d064d6c5020bd37
Dane Petersen <thegreatsunra@gmail.com>
2022-10-17 08:36:43 -0500

    Loop mariadb block, rename loop vars, reformat loop variable

--- deploy/locals.tf
+++ deploy/locals.tf
@@ -1,25 +1,27 @@
 locals {
 
-  wp_loop_vars = {
-    wp_containers = {
-      "wp_blog" = "blog.thegreatsunra.net"
-    }
-    email_address = "thegreatsunra@gmail.com"
+  sites_available = jsondecode(file("./sites-available.json"))
+  dns             = jsondecode(file("./dns.json"))
+
+  container_loop = {
+    sites_available  = local.sites_available
+    dns              = local.dns
+    email_address    = var.email_address
+    database_prefix  = "db_"
+    wordpress_prefix = "wp_"
   }
 
-  sites_available_list = jsondecode(file("./sites-available.json"))
-
   user_data_vars = {
-    docker_compose_base64 = base64encode(templatefile("./docker/docker-compose.yml.tftpl", local.wp_loop_vars))
-    nginx_conf_base64                  = base64encode(templatefile("./docker/nginx/conf.d/default.conf.tftpl", local.wp_loop_vars))
-    nginx_config_php_conf_base64                  = base64encode(templatefile("./docker/nginx/conf.d/modules/php.conf.tftpl", local.wp_loop_vars))
+    docker_compose_base64              = base64encode(templatefile("./docker/docker-compose.yml.tftpl", local.container_loop))
+    nginx_conf_base64                  = base64encode(templatefile("./docker/nginx/conf.d/default.conf.tftpl", local.container_loop))
+    nginx_config_php_conf_base64       = base64encode(templatefile("./docker/nginx/conf.d/modules/php.conf.tftpl", local.container_loop))
     nginx_config_general_conf_base64   = base64encode(file("./docker/nginx/conf.d/modules/general.conf"))
     nginx_config_security_conf_base64  = base64encode(file("./docker/nginx/conf.d/modules/security.conf"))
     nginx_config_wordpress_conf_base64 = base64encode(file("./docker/nginx/conf.d/modules/wordpress.conf"))
     nginx_dockerfile                   = base64encode(file("./docker/nginx/Dockerfile"))
     public_ssh_key                     = var.public_ssh_key
     root_temporary_password            = var.root_temporary_password
-    tmp_folder                         = local.tmp_folder
+    tmp_folder_path                    = var.tmp_folder_path
     user_full_name                     = var.user_full_name
     user_temporary_password            = var.user_temporary_password
     username                           = var.username
