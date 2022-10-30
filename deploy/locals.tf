0d077566abe8bb7806841b33a356e8173ef02ec6
Dane Petersen <thegreatsunra@gmail.com>
2022-10-29 19:52:43 -0500

    Run terraform fmt

--- deploy/locals.tf
+++ deploy/locals.tf
@@ -1,7 +1,7 @@
 locals {
 
-  dns             = jsondecode(file("./dns.json"))
-  
+  dns = jsondecode(file("./dns.json"))
+
   sites_available = jsondecode(file("./sites-available.json"))
 
   container_loop = {
@@ -13,20 +13,20 @@ locals {
   }
 
   user_data_vars = {
-    docker_compose_base64              = base64encode(templatefile("./docker/docker-compose.yml.tftpl", local.container_loop))
-    droplet_hostname                   = var.droplet_hostname
-    nginx_conf_base64                  = base64encode(templatefile("./docker/nginx/conf.d/default.conf.tftpl", local.container_loop))
-    nginx_config_general_conf_base64   = base64encode(file("./docker/nginx/conf.d/modules/general.conf"))
-    nginx_config_php_conf_base64       = base64encode(templatefile("./docker/nginx/conf.d/modules/php.conf.tftpl", local.container_loop))
-    nginx_config_security_conf_base64  = base64encode(file("./docker/nginx/conf.d/modules/security.conf"))
-    nginx_config_wordpress_conf_base64 = base64encode(file("./docker/nginx/conf.d/modules/wordpress.conf"))
-    nginx_config_wordpress_multisite_conf_base64  = base64encode(file("./docker/nginx/conf.d/modules/wordpress-multisite.conf"))
-    public_ssh_key                     = var.public_ssh_key
-    root_temporary_password            = var.root_temporary_password
-    tmp_folder_path                    = var.tmp_folder_path
-    user_full_name                     = var.user_full_name
-    user_temporary_password            = var.user_temporary_password
-    username                           = var.username
-    wordpress_dotenv_base64            = base64encode(file("./docker/.env"))
+    docker_compose_base64                        = base64encode(templatefile("./docker/docker-compose.yml.tftpl", local.container_loop))
+    droplet_hostname                             = var.droplet_hostname
+    nginx_conf_base64                            = base64encode(templatefile("./docker/nginx/conf.d/default.conf.tftpl", local.container_loop))
+    nginx_config_general_conf_base64             = base64encode(file("./docker/nginx/conf.d/modules/general.conf"))
+    nginx_config_php_conf_base64                 = base64encode(templatefile("./docker/nginx/conf.d/modules/php.conf.tftpl", local.container_loop))
+    nginx_config_security_conf_base64            = base64encode(file("./docker/nginx/conf.d/modules/security.conf"))
+    nginx_config_wordpress_conf_base64           = base64encode(file("./docker/nginx/conf.d/modules/wordpress.conf"))
+    nginx_config_wordpress_multisite_conf_base64 = base64encode(file("./docker/nginx/conf.d/modules/wordpress-multisite.conf"))
+    public_ssh_key                               = var.public_ssh_key
+    root_temporary_password                      = var.root_temporary_password
+    tmp_folder_path                              = var.tmp_folder_path
+    user_full_name                               = var.user_full_name
+    user_temporary_password                      = var.user_temporary_password
+    username                                     = var.username
+    wordpress_dotenv_base64                      = base64encode(file("./docker/.env"))
   }
 }
